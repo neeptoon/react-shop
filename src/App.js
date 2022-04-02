c31f0b5e1b610cfedb94138e342fc2aeffb5e5e4
Evgenii Shekhanov <evgenii.shekhanov@convergent.digital>
2022-04-02 10:43:42 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Hello from "./Components/Hello";
+
+function App() {
+  return (
+    <div className="App">
+      <Hello></Hello>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Hello.jsx
@@ -0,0 +1,5 @@
+export default () => {
+	return (
+		<h2>Hello world</h2>
+	)
+}
